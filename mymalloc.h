54630d2416bd2578ad3c4641439ccbc656410df5
hassaansaeed84 <hassaansaeed84@gmail.com>
2019-03-05 12:44:07 -0500

    Made Header for now

---
+++ mymalloc.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+#define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
+#define free( x ) myfree( x, __FILE__, __LINE__ )
+
+static char myblock[4096]
+
+struct memEntry
+{
+int isFree;
+int size;
+int struct memEntry * next;
+}
+
