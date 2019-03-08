d76c693b72bb485a854c45d505645180ab6caa62
hassaansaeed84 <hassaansaeed84@gmail.com>
2019-03-07 23:50:08 -0500

    Refined malloc during office hours

--- mymalloc.c
+++ mymalloc.c
@@ -3,54 +3,65 @@
 void insert(struct memEntry * head, int data)
 {
  struct memEntry * temp = NULL;
- temp = (struct memEntry*)&myblock[data];
+ temp = myblock[data];
  temp->size = 4096-data;
  temp->isFree = 1;
  temp->next = NULL;
- head->next = temp;
+ head->next = temp;3
 }
 void* mymalloc(int x, char* file, int line) {
+	
 	struct memEntry * head = NULL;
 	short magic=1999;
-	int data = 0;
 	if (x <= 0){
 		printf("Enter a valid number to malloc\n");
 		return NULL;
-	} if ((x + sizeof(struct memEntry)) >= 4096) {
+	} if ((x + sizeof(struct memEntry)) >= 4096)
+	 {
 		printf("%d is too large\n");
-		return NULL;
-	} if ((x + data) >= 4096) {
-		printf("Not enough space left\n");
 		return NULL;
 	}
 	  if ( *(short *)myblock!= magic) {
-	  *(short *)&myblock[sizeof(short)]=magic;
-	  head = (struct memEntry*)&myblock[sizeof(struct memEntry)];
+	  *(short *)&myblock=magic;
+	  head = (struct memEntry*)&myblock; // myblock
 	  head->next = NULL;
-	  head->size = x;
-	  head->isFree=0;
-	  data = sizeof(struct memEntry) + sizeof(short) + head->size;
-	  insert(head, data);
+	  head->size = 4096 - sizeof(struct memEntry);
+	  head->isFree=1;
+	  //data = sizeof(struct memEntry) + sizeof(short) + head->size;
+	  //insert(head, data);
 	}
 	 
-	 struct memEntry* new = (struct memEntry*)&myblock[head->size + sizeof(struct memEntry)];
-	 new = head;
-	 while(new->isFree==1) {
-	 	if(new->size==x) {
+	 struct memEntry* new = head;
+	 while(new != NULL) {
+	 	if(new->size>=x && new->isFree==1) {
 	 	  new->isFree=0;
+	 	  int size_remain = new->size - x;	 	  
+	 	  new->size = x;
+	 	  
+	 	  if( size_remain > sizeof(struct memEntry) + 1 )
+	 	  {	 	  
+		 	  char* ptr = new;
+		 	  ptr = ptr + sizeof(struct memEntry);
+		 	  ptr = ptr + new->size;
+		 	  
+		 	  struct memEntry* next = (struct memEntry*) ptr;
+		 	  next->size = size_remain - sizeof(struct memEntry);
+		 	  next->next = new->next;
+		 	  new->next = next;
+	 	  }
 	 	  break;
 	 	}
-	 	else if(new->size > x) {
-	 	 int temp = new->size;
-	 	 new->size=x;
-	 	 insert(new, data);
-	 	 new->isFree=0;
-	 	 break;
-	 	}
 	 new = new->next;
-	 data = sizeof(struct memEntry) + new->size;
 	}
-	return (void *)(new + sizeof(struct memEntry));
+	if(new==NULL)
+	{
+	 return NULL;
+	}
+	
+	char* ret = (char*)new;
+	ret = ret + sizeof(struct memEntry);
+	
+	return (void *)(ret);
 }
 
 void* myfree(int x, char* file, int line) {
